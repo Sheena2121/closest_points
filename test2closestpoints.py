bb7b8a10ea22d338a52dc95236d75634c8e9744c
Sheena2121 <96221843+Sheena2121@users.noreply.github.com>
2022-09-19 10:37:56 -0700

    handles multiple correct answers, fails large test

--- test2closestpoints.py
+++ test2closestpoints.py
@@ -16,26 +16,49 @@ class SmallCorrectnessEdgeCaseTests:
         if len(x_coords) > 1:
             result_brute_force = self.pointSet.find_closest_brute_force(self.pointSet.pointList)
             print(f"brute force: point1: ({result_brute_force[0].coords[0]},{result_brute_force[0].coords[1]}) point2: ({result_brute_force[1].coords[0]},{result_brute_force[1].coords[1]}), distance: {result_brute_force[2]}")
-
-        if len(x_coords) > 1:
             result_faster = self.pointSet.find_closest_faster(self.pointSet.pointList)
             print(f"faster: point1: ({result_faster[0].coords[0]},{result_faster[0].coords[1]}) point2: ({result_faster[1].coords[0]},{result_faster[1].coords[1]}), distance: {result_faster[2]}")
 
         else:
             print("looks like you need to add more points!")
 
-class ClosestPointsTest:
-    def __init__(self, set_size):
-        set = NearestPointSet()
+class MultipleCorrectAnswersEdgeCaseTests:
+    def __init__(self, x_coords, y_coords):
+        self.x_coords = x_coords
+        self.y_coords = y_coords
+        self.pointSet = NearestPointSet()
 
-        x_coords = np.random.randint(0, high=200000, size=set_size, dtype=int)
-        y_coords = np.random.randint(0, high=200000, size=set_size, dtype=int)
+        def create_plot():
+            for i in range(0, len(x_coords)):
+                self.pointSet.insert(Point(self.x_coords[i], self.y_coords[i]))
+        create_plot()
+
+        if self.pointSet.find_closest_brute_force(self.pointSet.pointList) == "multiple correct answers":
+            print("We have multiple correct answers here!")
+
+        else:
+            result_brute_force = self.pointSet.find_closest_brute_force(self.pointSet.pointList)
+            print(f"brute force: point1: ({result_brute_force[0].coords[0]},{result_brute_force[0].coords[1]}) point2: ({result_brute_force[1].coords[0]},{result_brute_force[1].coords[1]}), distance: {result_brute_force[2]}")
+            result_faster = self.pointSet.find_closest_faster(self.pointSet.pointList)
+            print(f"faster: point1: ({result_faster[0].coords[0]},{result_faster[0].coords[1]}) point2: ({result_faster[1].coords[0]},{result_faster[1].coords[1]}), distance: {result_faster[2]}")
+
+class LargeCorrectnessAndScalabilityTests:
+    def __init__(self, set_size):
+        self.pointSet = NearestPointSet()
+
+        x_coords = np.random.randint(0, high=20000, size=set_size, dtype=int)
+        y_coords = np.random.randint(0, high=20000, size=set_size, dtype=int)
         def create_plot(pointSet):
             for i in range(0, len(x_coords)):
                 pointSet.pointList.append(Point(x_coords[i], y_coords[i]))
 
-        create_plot(set)
+        create_plot(self.pointSet)
 
+        #if len(x_coords) < 10001:
+         #   result_brute_force = self.pointSet.find_closest_brute_force(self.pointSet.pointList)
+          #  print(f"brute force: point1: ({result_brute_force[0].coords[0]},{result_brute_force[0].coords[1]}) point2: ({result_brute_force[1].coords[0]},{result_brute_force[1].coords[1]}), distance: {result_brute_force[2]}")
+        result_faster = self.pointSet.find_closest_faster(self.pointSet.pointList)
+        print(f"faster: point1: ({result_faster[0].coords[0]},{result_faster[0].coords[1]}) point2: ({result_faster[1].coords[0]},{result_faster[1].coords[1]}), distance: {result_faster[2]}")
 
 
 #small_correct_test1 = SmallCorrectnessEdgeCaseTests([3, 3, 7, 8], [3, 3, 4, 6])
@@ -44,22 +67,18 @@ class ClosestPointsTest:
 #small_correct_test4 = SmallCorrectnessEdgeCaseTests([4, 7, 9000, 70, 2590, 32, 16], [4, 3, 90, 725, 22500, 10, 11])
 #small_correct_test5 = SmallCorrectnessEdgeCaseTests([2, 67, 35, 42, 17, 22, 3897], [2, 45, 25, 9890, 22311, 78961, 250])
 
-edge_case_0_points = SmallCorrectnessEdgeCaseTests([], [])
-edge_case_1_point = SmallCorrectnessEdgeCaseTests([5], [2])
-edge_case_2_points = SmallCorrectnessEdgeCaseTests([3, 4], [2, 7])
-edge_case_negative_numbers = SmallCorrectnessEdgeCaseTests([-3, 0, 5], [-2, 0, -1])
-edge_case_multiple_options_for_correct_answer = SmallCorrectnessEdgeCaseTests([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+#edge_case_0_points = SmallCorrectnessEdgeCaseTests([], [])
+#edge_case_1_point = SmallCorrectnessEdgeCaseTests([5], [2])
+#edge_case_2_points = SmallCorrectnessEdgeCaseTests([3, 4], [2, 7])
+#edge_case_negative_numbers = SmallCorrectnessEdgeCaseTests([-3, 0, 5], [-2, 0, -1])
+#edge_case_multiple_options_for_correct_answer = MultipleCorrectAnswersEdgeCaseTests([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
 
 
+#points_test10 = LargeCorrectnessAndScalabilityTests(10)
 
-#points_test0 = ClosestPointsTest(0)
-#points_test1 = ClosestPointsTest(1)
-#points_test1 = ClosestPointsTest(2)
-#points_test10 = ClosestPointsTest(10)
+#points_test100 = LargeCorrectnessAndScalabilityTests(100)
+#points_test10000 = LargeCorrectnessAndScalabilityTests(10000)
+#points_test10002 = LargeCorrectnessAndScalabilityTests(10002)
 
-#points_test100 = ClosestPointsTest(100)
-
-#points_test1000 = ClosestPointsTest(1000)
-
-#points_test1000000 = ClosestPointsTest(1000000)
+#points_test10000 = LargeCorrectnessAndScalabilityTests(10000)
 
